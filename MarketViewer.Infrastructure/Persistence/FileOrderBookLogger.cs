8d08281737e969ebd7ba7fc634cf0731f86d40c6
Stanislav <zorg.msv@gmail.com>
2024-07-31 11:12:47 +0300

    Added Infrastructure layer

---
+++ MarketViewer.Infrastructure/Persistence/FileOrderBookLogger.cs
@@ -0,0 +1,18 @@
+﻿using MarketViewer.Domain.Abstractions;
+using MarketViewer.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketViewer.Infrastructure.Persistence
+{
+	public class FileOrderBookLogger : IOrderBookLogger
+	{
+		public Task Log(OrderBook orderBook)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ MarketViewer.Infrastructure/Persistence/InMemoryOrderBookLogger.cs
@@ -0,0 +1,30 @@
+﻿using MarketViewer.Domain.Abstractions;
+using MarketViewer.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketViewer.Infrastructure.Persistence
+{
+	/// <summary>
+	/// InMemory implementation of <see cref="IOrderBookLogger"/> for debugging and demonstration
+	/// </summary>
+	public class InMemoryOrderBookLogger : IOrderBookLogger
+	{
+		private readonly List<OrderBook> _loggedOrderBooks;
+
+		public InMemoryOrderBookLogger()
+		{
+			_loggedOrderBooks = new();
+		}
+
+		public Task Log(OrderBook orderBook)
+		{
+			_loggedOrderBooks.Add(orderBook);
+
+			return Task.CompletedTask;
+		}
+	}
+}
